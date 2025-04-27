16211acd259b3430d6bc1de70f5019381b5db8fb
Venefilyn <freya@venefilyn.se>
2025-04-27 02:29:12 +0200

    chore: Consolidated layered packages

--- build_files/desktop-packages.sh
+++ build_files/desktop-packages.sh
@@ -9,54 +9,52 @@ dnf5 -y copr enable "${COPR_REPOS[@]}"
 
 # Layered Applications
 LAYERED_PACKAGES=(
+    ansible
+    atuin
+    bat
     borgbackup
+    btop
+    bun
+    cheat
     cockpit
     cockpit-machines
     cockpit-ostree
     cockpit-sosreport
-    fish
-    ghostty
-    grc
-    krb5-workstation
-    libinput-devel
-    libva-utils
-    libvirt vagrant
-    lld
-    podman-compose
-    podman-remote
-    virt-install
-)
-
-# From nix
-LAYERED_PACKAGES+=(
-    ansible
-    atuin
-    bat
-    btop
-    bun
-    cheat
     cosign
     devpod
     devpod-desktop
     direnv
     eza
     fira-code-fonts
+    fish
     gh
+    ghostty
     gphoto2
+    grc
     helix
+    krb5-workstation
+    libinput-devel
+    libva-utils
+    libvirt vagrant
+    lld
     neovim
     nodejs
+    podman-compose
+    podman-remote
     pre-commit
     rbw
     rclone
     ripgrep
     sqlite
     starship
+    thefuck
+    virt-install
     yq
     yubikey-manager
     zoxide
     zsh
 )
+
 
 dnf5 install --setopt=install_weak_deps=False -y "${LAYERED_PACKAGES[@]}"
 
