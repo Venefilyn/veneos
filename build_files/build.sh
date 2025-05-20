bb8211203f37fe3750ff8090ed5c7a6bfe6d63d1
Venefilyn <freya@venefilyn.se>
2025-05-20 22:55:24 +0200

    feat: Group logs in GitHub logs
    Signed-off-by: Venefilyn <freya@venefilyn.se>

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,6 +4,18 @@ set ${SET_X:+-x} -eou pipefail
 
 trap '[[ $BASH_COMMAND != echo* ]] && [[ $BASH_COMMAND != log* ]] && echo "+ $BASH_COMMAND"' DEBUG
 
+function echo_group() {
+    local WHAT
+    WHAT="$(
+        basename "$1" .sh |
+            tr "-" " " |
+            tr "_" " "
+    )"
+    echo "::group:: == ${WHAT^^} =="
+    "$1"
+    echo "::endgroup::"
+}
+
 log() {
   echo "== $* =="
 }
@@ -11,17 +23,17 @@ log() {
 log "Starting VeneOS build process - Inspired by AmyOS and m2os"
 
 log "Enable container signing"
-/ctx/signing.sh
+echo_group /ctx/signing.sh
 
 case "$BASE_IMAGE" in
 *"/bazzite"*)
-    /ctx/desktop-packages.sh
-    /ctx/just-files.sh
-    /ctx/desktop-defaults.sh
+    echo_group /ctx/desktop-packages.sh
+    echo_group /ctx/just-files.sh
+    echo_group /ctx/desktop-defaults.sh
     ;;
 *"/ucore"*) ;;
 esac
 
 
 log "Post build cleanup"
-/ctx/cleanup.sh
+echo_group /ctx/cleanup.sh
