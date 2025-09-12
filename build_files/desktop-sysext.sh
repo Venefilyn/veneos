8d8bd33d5b20c6a8151216dbb12a963d67190349
Freya Gustavsson <freya@venefilyn.se>
2025-09-12 15:19:27 +0200

    feat: Add system extensions
    To start with it adds from travier's repo
    - bitwarden
    - tailscale
    - vscodium
    
    See-also: https://github.com/travier/fedora-sysexts
    Signed-off-by: Freya Gustavsson <freya@venefilyn.se>

---
+++ build_files/desktop-sysext.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+mkdir -p /usr/lib/sysupdate.d
+
+SYSEXTS_TRAVIER=(
+  bitwarden
+  tailscale
+  vscodium
+)
+for s in "${SYSEXTS_TRAVIER[@]}"; do
+    tee /usr/lib/sysupdate.d/"$s".transfer <<EOF
+[Transfer]
+Verify=false
+
+[Source]
+Type=url-file
+Path=https://extensions.fcos.fr/extensions/$s/
+MatchPattern=$s-@v-%w-%a.raw
+
+[Target]
+InstancesMax=2
+Type=regular-file
+Path=/var/lib/extensions.d/
+MatchPattern=$s-@v-%w-%a.raw
+CurrentSymlink=/var/lib/extensions/$s.raw
+EOF
+done
